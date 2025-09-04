d91af58e75f92e494241136e0b44a39be9cdc81f
CalvinH-Dev <hanisch.calvin@gmail.com>
2025-09-04 19:50:27 +0200

    Added the aside to the end of the page and created a footer component
    The aside marks the end of the page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 import { Menu } from "./components/menu/menu";
 
 @Component({
 	selector: "app-root",
-	imports: [RouterOutlet, Header, Menu],
+	imports: [RouterOutlet, Header, Menu, Footer],
 	templateUrl: "./app.html",
 	styleUrl: "./app.scss",
 })
