40042ca21a3bf98ee68f68750d1b94d848293366
CalvinH-Dev <hanisch.calvin@gmail.com>
2025-10-29 16:22:50 +0100

    legal notice implemented, now everything implemented.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { Home } from "./components/pages/home/home";
+import { Legal } from "./components/pages/legal/legal";
 import { Privacy } from "./components/pages/privacy/privacy";
 
 export const routes: Routes = [
 	{ path: "", component: Home },
 	{ path: "privacy", component: Privacy },
+	{ path: "legal", component: Legal },
 ];
---
+++ src/app/components/pages/legal/legal.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from "@angular/core";
+import { LinkArrow } from "app/components/link-arrow/link-arrow";
+import { End } from "app/components/sections/end/end";
+import { LanguageService } from "app/services/language";
+
+@Component({
+	selector: "app-legal",
+	imports: [End, LinkArrow],
+	templateUrl: "./legal.html",
+	styleUrl: "./legal.scss",
+})
+export class Legal {
+	languageService = inject(LanguageService);
+	language = this.languageService.getLanguage();
+}
