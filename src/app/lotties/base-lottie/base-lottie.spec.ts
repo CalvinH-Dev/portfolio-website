b30bb72c57d1f40dd2168d24758636dcb9df4d29
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-01-22 12:53:36 +0100

    update: Angular v21 installed and updated all packages except for jasmine. Also formatted whole codebase with prettier:write.

--- src/app/lotties/base-lottie/base-lottie.spec.ts
+++ src/app/lotties/base-lottie/base-lottie.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { BaseLottie } from './base-lottie';
+import { BaseLottie } from "./base-lottie";
 
-describe('BaseLottie', () => {
-  let component: BaseLottie;
-  let fixture: ComponentFixture<BaseLottie>;
+describe("BaseLottie", () => {
+	let component: BaseLottie;
+	let fixture: ComponentFixture<BaseLottie>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BaseLottie]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [BaseLottie],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(BaseLottie);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(BaseLottie);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
